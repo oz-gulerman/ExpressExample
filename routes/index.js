7c9d5057648048f03ba55938e301a6b708778b9b
Oz Gulerman <oz_gulerman@gap.com>
2020-04-06 02:04:00 -0700

    fix(003)[OZ]: Exclude .env files from git commits

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
+const Registration = mongoose.model('Registration');
 
 router.get('/', (req, res) => {
   res.render('form', { title: 'Registration form' });
@@ -20,7 +22,13 @@ router.post('/',
     const errors = validationResult(req);
 
     if (errors.isEmpty()) {
-      res.send('Thank you for your registration!');
+      const registration = new Registration(req.body);
+      registration.save()
+        .then(() => { res.send('Thank you for your registration!'); })
+        .catch((err) => {
+          console.log(err);
+          res.send('Sorry! Something went wrong.');
+        });
     } else {
       res.render('form', {
         title: 'Registration form',
