dba2c4c682d23e227ed920c5c61ba00174ac58d9
Oz Gulerman <oz_gulerman@gap.com>
2020-04-06 01:48:34 -0700

    feat(003)[OZ]: Integrate MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
 
 module.exports = app;
