4be1653eaf1299e461fda02384e2748477e5fc2b
Oz Gulerman <oz_gulerman@gap.com>
2020-04-06 03:38:29 -0700

    fix(008)[OZ]: Make PORT an env variable

--- start.js
+++ start.js
@@ -17,6 +17,10 @@ mongoose.connection
 require('./models/Registration');
 
 const app = require('./app');
-const server = app.listen(3000, () => {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+const server = app.listen(port, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
