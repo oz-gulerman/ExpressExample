f56094b4f395f7ee12d2971d4514a072345502b5
Oz Gulerman <oz_gulerman@gap.com>
2020-04-06 02:12:11 -0700

    feat(004)[OZ]: Add /registrations route

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,14 @@ router.get('/', (req, res) => {
   res.render('form', { title: 'Registration form' });
 });
 
+router.get('/registrations', (req, res) => {
+  Registration.find()
+    .then((registrations) => {
+      res.render('index', { title: 'Listing registrations', registrations });
+    })
+    .catch(() => { res.send('Sorry! Something went wrong.'); });
+});
+
 router.post('/',
   [
     check('name')
